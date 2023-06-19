86ed793caaad17b46da3c42a9d213e08ba8a724e
Clay Dowling <clay@lazarusid.com>
2023-06-19 11:02:38 -0400

    Can find camera card on its own

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,16 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <vector>
+
+#include <fileapi.h>
 
 using std::cout;
 using std::endl;
 using std::find;
 using std::ifstream;
+using std::ostringstream;
 using std::string;
 using std::filesystem::file_size;
 using std::filesystem::path;
@@ -26,90 +30,115 @@ using std::chrono::system_clock;
 ConcurrentQueue<copyCommand> files;
 vector<string> missingDirectories;
 
+#define _BV(bit) (1 << (bit))
+
+
 void loadFiles(path s) {
-  for (auto e : recursive_directory_iterator(s)) {
-    char datestamp[12];
-    const auto lastwrite = std::filesystem::last_write_time(e);
-    const auto systemTime = std::chrono::file_clock::to_sys(lastwrite);
+    for (auto e: recursive_directory_iterator(s)) {
 
-    path p(e);
+        path p(e);
 
-    if (std::filesystem::is_regular_file(p)) {
-      copyCommand v;
-      v.source = p;
-      v.size = file_size(p);
+        if (std::filesystem::is_regular_file(p)) {
 
-      const std::chrono::year_month_day ymd(floor<days>(systemTime));
+            auto root = get_destination(p);
+            if (root == NOPATH) {
+                if (find(missingDirectories.begin(), missingDirectories.end(), p.extension().string()) ==
+                    missingDirectories.end()) {
+                    missingDirectories.push_back(p.extension().string());
+                }
+            } else {
+                char datestamp[12];
+                const auto lastwrite = std::filesystem::last_write_time(e);
+                const auto systemTime = std::chrono::file_clock::to_sys(lastwrite);
 
-      int y = (int)ymd.year();
-      unsigned m = (unsigned)ymd.month();
-      unsigned d = (unsigned)ymd.day();
+                copyCommand v;
+                v.source = p;
+                v.size = file_size(p);
 
-      snprintf(datestamp, sizeof(datestamp), "%4.4d-%2.2d-%2.2d", y, m, d);
+                const std::chrono::year_month_day ymd(floor<days>(systemTime));
 
-      auto root = get_destination(p);
-      if (root == NOPATH) {
-        if (find(missingDirectories.begin(), missingDirectories.end(),
-                 p.extension().string()) == missingDirectories.end()) {
-          missingDirectories.push_back(p.extension().string());
+                int y = (int) ymd.year();
+                unsigned m = (unsigned) ymd.month();
+                unsigned d = (unsigned) ymd.day();
+
+                snprintf(datestamp, sizeof(datestamp), "%4.4d-%2.2d-%2.2d", y, m, d);
+                v.destination = root / datestamp / p.filename();
+                files.push(v);
+            }
         }
-      } else {
-        v.destination = root / datestamp / p.filename();
-        files.push(v);
-      }
     }
-  }
 }
 
 void read_configuration(path configfile) {
-  string extension;
-  string destination;
-  ifstream in(configfile.c_str());
+    string extension;
+    string destination;
+    ifstream in(configfile.c_str());
 
-  string content((std::istreambuf_iterator<char>(in)),
-                 (std::istreambuf_iterator<char>()));
-  in.close();
+    string content((std::istreambuf_iterator<char>(in)), (std::istreambuf_iterator<char>()));
+    in.close();
 
-  Parser configparser;
-  auto filemap = configparser.Parse(content);
-  for(auto p: filemap) {
-      if (p.second == "IGNORE") {
-          add_destination(p.first, IGNORE_EXTENSION);
-      } else {
-          add_destination(p.first, p.second);
-      }
-  }
+    Parser configparser;
+    auto filemap = configparser.Parse(content);
+    for (auto p: filemap) {
+        if (p.second == "IGNORE") {
+            add_destination(p.first, IGNORE_EXTENSION);
+        } else {
+            add_destination(p.first, p.second);
+        }
+    }
 
+}
+
+string find_start_path() {
+    auto drives = GetLogicalDrives();
+
+    for (int i = 0; i < 26; ++i) {
+        DWORD mask = _BV(i);
+        if (drives & mask) {
+            ostringstream d;
+            d << (char) ('A' + i) << ":\\DCIM";
+            cout << "Trying " << d.str() << endl;
+            if (std::filesystem::exists(d.str())) {
+                return d.str();
+            }
+        }
+    }
+
+    return ".";
 }
 
 int main(int argc, const char *argv[]) {
 
-  path homedir = getenv("USERPROFILE");
-  path configfile = homedir / ".photounloader";
+    path homedir = getenv("USERPROFILE");
+    path configfile = homedir / ".photounloader";
 
-  cout << "Reading from " << configfile.string() << endl;
-  read_configuration(configfile);
+    cout << "Reading from " << configfile.string() << endl;
+    read_configuration(configfile);
 
-  string startpath = ".";
-  if (argc > 1) {
-    startpath = argv[1];
-  }
-  loadFiles(startpath);
-
-  try {
-    for (auto d = files.frontAndPop();; d = files.frontAndPop()) {
-      cout << d.destination.string() << endl;
+    string startpath = ".";
+    if (argc > 1) {
+        startpath = argv[1];
+    } else {
+        cout << "Looking for drive" << endl;
+        startpath = find_start_path();
+        cout << "Found " << startpath << endl;
     }
-  } catch (std::out_of_range e) {
-    cout << "List complete." << endl;
-  }
+    loadFiles(startpath);
 
-  if (!missingDirectories.empty()) {
-    cout << "Missing extensions:" << endl;
-    for (auto m : missingDirectories) {
-      cout << m << endl;
+    try {
+        for (auto d = files.frontAndPop();; d = files.frontAndPop()) {
+            cout << d.destination.string() << endl;
+        }
+    } catch (std::out_of_range e) {
+        cout << "List complete." << endl;
     }
-  }
 
-  return EXIT_SUCCESS;
+    if (!missingDirectories.empty()) {
+        cout << "Missing extensions:" << endl;
+        for (auto m: missingDirectories) {
+            cout << m << endl;
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
